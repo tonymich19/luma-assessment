2cb8d7a848a3b3a6323820592ebfd1050f8e47c2
unknown <tony.correia19@outlook.com>
2024-11-24 17:29:52 -0300

    adding mochawave config

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+        reportDir: 'cypress/reports',
+        overwrite: false,
+        html: true,
+        json: true,
+        reportPageTitle: 'Cypress Test Report',
+    },
   e2e: {
     experimentalSessionAndOrigin: true,
     baseUrl: 'https://www.lumahealth.io/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+            return config;
     },
   },
 });
