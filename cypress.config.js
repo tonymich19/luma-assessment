6a10171bb7a27b70cbf91813c8ab0bf0a00ff7a6
unknown <tony.correia19@outlook.com>
2024-11-24 19:42:46 -0300

    more github actions config

--- cypress.config.js
+++ cypress.config.js
@@ -2,19 +2,23 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-        reportDir: 'cypress/reports',
-        overwrite: false,
-        html: true,
-        json: true,
-        reportPageTitle: 'Cypress Test Report',
-    },
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+    video: true,
+    reportPageTitle: 'Cypress Test Report',
+  },
+  video: true,
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
   e2e: {
     experimentalSessionAndOrigin: true,
     baseUrl: 'https://www.lumahealth.io/',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
-            return config;
+      return config;
     },
   },
 });
